2b238e02413f50cb5c35eb25d45075e230ff1bec
Wenwen123-Ban <galagalerwin140@gmail.com>
2026-05-10 07:17:03 +0800

    describe what you changed here

--- src/core/generate_admin_hash.py
+++ src/core/generate_admin_hash.py
@@ -1,5 +1,7 @@
 """Generate one-time admin password and physical recovery-key hashes."""
-
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 import secrets
 import string
 
